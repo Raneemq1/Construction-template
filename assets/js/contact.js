846b62f84b89405151a2d623cbf8dae6f5818e60
Raneemq1 <raneemalqadi32@gmail.com>
2023-01-27 03:37:34 +0200

    add loading screen and dropdown lists

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -18,3 +18,21 @@ let up=document.getElementById("move-to-up");
       behavior:"smooth"
     })
   });
+   /* Loading icon */
+
+  //remove scroll form body using overflow
+  let body=document.body;
+  body.style.overflowY="hidden"
+
+  let load=document.getElementById("loading");
+  load.style.visibility="visible";
+  load.style.opacity="1";
+  //set the loading screen for short period
+  window.addEventListener('load',function(){
+    this.setTimeout(function(){
+      load.style.visibility="hidden";
+      load.style.opacity="0";
+      load.style.transition="1s"
+      body.style.overflowY="auto"
+    },2000)
+  })
--- assets/js/main.js
+++ assets/js/main.js
@@ -94,3 +94,22 @@ emailInput.addEventListener("blur", function(event) {
       behavior:"smooth"
     })
   });
+
+  /* Loading icon */
+
+  //remove scroll form body using overflow
+  let body=document.body;
+  body.style.overflowY="hidden"
+
+  let load=document.getElementById("loading");
+  load.style.visibility="visible";
+  load.style.opacity="1";
+  //set the loading screen for short period
+  window.addEventListener('load',function(){
+    this.setTimeout(function(){
+      load.style.visibility="hidden";
+      load.style.opacity="0";
+      load.style.transition="1s"
+      body.style.overflowY="auto"
+    },2000)
+  })
