632106d81987d91a6057a55bfe341315af85a5da
MayarAbuzahra <mayarabuzahra@hotmail.com>
2023-02-05 11:53:25 +0200

    element

--- assets/js/element.js
+++ assets/js/element.js
@@ -57,22 +57,137 @@ document.getElementById("s-txt").addEventListener("click", function () {
 });
 
 // Get the modal
-var modal = document.getElementById("myModal");
-
+let modal = document.getElementById("myModal");
+// Get the modal
+let modal_0 = document.getElementById("myModal_0");
+let modal_1 = document.getElementById("myModal_1");
+let modal_2 = document.getElementById("myModal_2");
+let modal_3 = document.getElementById("myModal_3");
+let modal_4 = document.getElementById("myModal_4");
+let modal_5 = document.getElementById("myModal_5");
+let modal_6 = document.getElementById("myModal_6");
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
+let img = document.getElementById("myImg");
+let modalImg = document.getElementById("img01");
+let img_1 = document.getElementById("myImg_1");
+let modalImg_1 = document.getElementById("img02");
+let img_2 = document.getElementById("myImg_2");
+let modalImg_2 = document.getElementById("img03");
+let img_3 = document.getElementById("myImg_3");
+let modalImg_3 = document.getElementById("img04");
+let img_4 = document.getElementById("myImg_4");
+let modalImg_4 = document.getElementById("img05");
+let img_5 = document.getElementById("myImg_5");
+let modalImg_5 = document.getElementById("img06");
+let img_6 = document.getElementById("myImg_6");
+let modalImg_6 = document.getElementById("img07");
+let img_7 = document.getElementById("myImg_7");
+let modalImg_7 = document.getElementById("img08");
+let captionText = document.getElementById("caption");
+img.onclick = function () {
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
-}
+};
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
+
+img_1.onclick = function () {
+  modal_0.style.display = "block";
+  modalImg_1.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_1 = document.getElementsByClassName("close_0")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_1.onclick = function () {
+  modal_0.style.display = "none";
+};
+
+img_2.onclick = function () {
+  modal_1.style.display = "block";
+  modalImg_2.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_2 = document.getElementsByClassName("close_1")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_2.onclick = function () {
+  modal_1.style.display = "none";
+};
+img_3.onclick = function () {
+  modal_2.style.display = "block";
+  modalImg_3.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_3 = document.getElementsByClassName("close_2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_3.onclick = function () {
+  modal_2.style.display = "none";
+};
+
+img_4.onclick = function () {
+  modal_3.style.display = "block";
+  modalImg_4.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_4 = document.getElementsByClassName("close_3")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_4.onclick = function () {
+  modal_3.style.display = "none";
+};
+img_5.onclick = function () {
+  modal_4.style.display = "block";
+  modalImg_5.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_5 = document.getElementsByClassName("close_4")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_5.onclick = function () {
+  modal_4.style.display = "none";
+};
+img_6.onclick = function () {
+  modal_5.style.display = "block";
+  modalImg_6.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_6 = document.getElementsByClassName("close_5")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_6.onclick = function () {
+  modal_5.style.display = "none";
+};
+img_7.onclick = function () {
+  modal_6.style.display = "block";
+  modalImg_7.src = this.src;
+  captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+let span_7 = document.getElementsByClassName("close_6")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_7.onclick = function () {
+  modal_6.style.display = "none";
+};
